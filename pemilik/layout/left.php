858857f869aa555330d74620c6baffd0a3f97836
melikacandra <85892259+melikacandra@users.noreply.github.com>
2021-06-15 21:09:48 +0700

    fitur2 pemilik

---
+++ pemilik/layout/left.php
@@ -0,0 +1,17 @@
+</div>
+        <div class="left">
+            <i class= "fa fa-home"></i>
+            <br>
+            <i class= "fa fa-road"></i>
+            <br>
+            <i class= "fa fa-newspaper-o"></i>
+            <div class="down">
+                <hr>
+                <i class= "fa fa-user-circle-o"></i>
+                <br>
+                <i class= "fa fa-sign-out"></i>
+            </div>
+        </div>
+    </head>
+    <body>
+        <div class="content">
